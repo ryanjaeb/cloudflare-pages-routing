43010e92ecacf27faddea84c881c0c020e30ab3e
Ryan Jaeb <ryan@jaeb.ca>
2021-11-25 06:36:28 -0600

    Delete hellopages.js

--- functions/hellopages.js
+++
@@ -1,6 +0,0 @@
-// noinspection JSUnusedGlobalSymbols,JSUnusedLocalSymbols
-export async function onRequestGet(context) {
-    const res = new Response("Hello Pages!")
-    res.headers.set("Content-Type", "text/plain")
-    return res;
-}
